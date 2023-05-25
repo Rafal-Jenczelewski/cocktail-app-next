c61357d7e60d16f3ec0b149cadc49129b20f5cf2
rjenc <r.jenczelewski@gmail.com>
2023-05-25 21:41:59 +0200

    Prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,30 @@
 import Link from "next/link";
 
-const routes = [{url: "/ingredients", label: "Skadniki"}, {url: "/recipes", label: "Przepisy"}];
+const routes = [
+  { url: "/ingredients", label: "Skadniki" },
+  { url: "/recipes", label: "Przepisy" },
+];
 
 export default function Home() {
-    return (
-      <main className={"flex h-full w-full flex-col items-center justify-start"}>
-        <h1 className="px-4 pt-16 text-center text-6xl font-extrabold tracking-tight">
+  return (
+    <main className={"flex h-full w-full flex-col items-center justify-start"}>
+      <h1 className="px-4 pt-16 text-center text-6xl font-extrabold tracking-tight">
         <span className="block uppercase text-primary-text drop-shadow-md">
           Apka do drinków
         </span>
-        </h1>
-        <div className="my-auto flex h-64 w-full flex-col items-center justify-evenly">
-          {routes.map((link) => (
-              <div key={link.url} className="mx-32 w-80 max-w-sm">
-                <Link
-                    href={link.url}
-                    className="flex grow-1 items-center justify-center rounded-md border border-transparent bg-primary-bg px-4 py-3 text-xl font-medium text-white shadow-xl active:bg-yellow-800 sm:px-8"
-                >
-                  {link.label}
-                </Link>
-              </div>
-          ))}
-        </div>
-      </main>
-  )
+      </h1>
+      <div className="my-auto flex h-64 w-full flex-col items-center justify-evenly">
+        {routes.map((link) => (
+          <div key={link.url} className="mx-32 w-80 max-w-sm">
+            <Link
+              href={link.url}
+              className="grow-1 flex items-center justify-center rounded-md border border-transparent bg-primary-bg px-4 py-3 text-xl font-medium text-white shadow-xl active:bg-yellow-800 sm:px-8"
+            >
+              {link.label}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 }
