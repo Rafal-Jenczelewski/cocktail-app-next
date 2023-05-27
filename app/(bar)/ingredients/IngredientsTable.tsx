5ee02b26b1f7d5d6d52be902e5a337fce348e0db
rjenc <r.jenczelewski@gmail.com>
2023-05-27 20:27:16 +0200

    Refactor table

--- app/(bar)/ingredients/IngredientsTable.tsx
+++ app/(bar)/ingredients/IngredientsTable.tsx
@@ -6,6 +6,7 @@ import {
   getCoreRowModel,
   getSortedRowModel,
   SortingState,
+  Table,
 } from "@tanstack/table-core";
 import { flexRender, useReactTable } from "@tanstack/react-table";
 import { useState, useTransition } from "react";
@@ -14,24 +15,6 @@ import { FaSortAmountUp, FaSortAmountDown } from "react-icons/fa";
 import { editIngredient } from "@/app/(bar)/ingredients/actions";
 
 const columnHelper = createColumnHelper<Ingredient>();
-
-function AvailabilityCell(props) {
-  const [isPending, startTransition] = useTransition();
-
-  return (
-    <input
-      type={"checkbox"}
-      disabled={isPending}
-      checked={props.getValue()}
-      onChange={(e) => {
-        startTransition(() =>
-          editIngredient(props.row.id, { available: e.target.checked })
-        );
-      }}
-      name={"availability"}
-    />
-  );
-}
 
 const columns = [
   columnHelper.accessor("name", {
@@ -51,7 +34,7 @@ export function IngredientsTable(props: { ingredients: Ingredient[] }) {
     { desc: true, id: "available" },
   ]);
 
-  const table = useReactTable({
+  const table = useReactTable<Ingredient>({
     columns,
     data: props.ingredients,
     getCoreRowModel: getCoreRowModel(),
@@ -61,42 +44,12 @@ export function IngredientsTable(props: { ingredients: Ingredient[] }) {
     },
     getSortedRowModel: getSortedRowModel(),
     getRowId: (row) => row.id,
-  });
+  }) as Table<Ingredient>; //TS????
 
   return (
     <div className={"h-[85vh] w-full overflow-y-scroll px-2"}>
       <table className={"w-full"}>
-        <thead className={"text-lg"}>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id} className={"grid grid-cols-[3fr_1fr]"}>
-              {headerGroup.headers.map((header) => (
-                <th key={header.id}>
-                  {header.isPlaceholder ? null : (
-                    <div
-                      className={cx(
-                        {
-                          "cursor-pointer select-none":
-                            header.column.getCanSort(),
-                        },
-                        "flex items-center justify-start gap-2"
-                      )}
-                      onClick={header.column.getToggleSortingHandler()}
-                    >
-                      {flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                      {{
-                        asc: <FaSortAmountUp />,
-                        desc: <FaSortAmountDown />,
-                      }[header.column.getIsSorted() as string] ?? null}
-                    </div>
-                  )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
+        <Header table={table} />
         <tbody>
           {table.getRowModel().rows.map((row) => (
             <tr key={row.id} className={"grid grid-cols-[2fr_1fr]"}>
@@ -117,23 +70,81 @@ export function IngredientsTable(props: { ingredients: Ingredient[] }) {
             </tr>
           ))}
         </tbody>
-        <tfoot>
-          {table.getFooterGroups().map((footerGroup) => (
-            <tr key={footerGroup.id}>
-              {footerGroup.headers.map((header) => (
-                <th key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.footer,
-                        header.getContext()
-                      )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </tfoot>
+        <Footer table={table} />
       </table>
     </div>
+  );
+}
+
+function Header(props: { table: Table<Ingredient> }) {
+  return (
+    <thead className={"text-lg"}>
+      {props.table.getHeaderGroups().map((headerGroup) => (
+        <tr key={headerGroup.id} className={"grid grid-cols-[3fr_1fr]"}>
+          {headerGroup.headers.map((header) => (
+            <th key={header.id}>
+              {header.isPlaceholder ? null : (
+                <div
+                  className={cx(
+                    {
+                      "cursor-pointer select-none": header.column.getCanSort(),
+                    },
+                    "flex items-center justify-start gap-2"
+                  )}
+                  onClick={header.column.getToggleSortingHandler()}
+                >
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext()
+                  )}
+                  {{
+                    asc: <FaSortAmountUp />,
+                    desc: <FaSortAmountDown />,
+                  }[header.column.getIsSorted() as string] ?? null}
+                </div>
+              )}
+            </th>
+          ))}
+        </tr>
+      ))}
+    </thead>
+  );
+}
+
+function Footer(props: { table: Table<Ingredient> }) {
+  return (
+    <tfoot>
+      {props.table.getFooterGroups().map((footerGroup) => (
+        <tr key={footerGroup.id}>
+          {footerGroup.headers.map((header) => (
+            <th key={header.id}>
+              {header.isPlaceholder
+                ? null
+                : flexRender(
+                    header.column.columnDef.footer,
+                    header.getContext()
+                  )}
+            </th>
+          ))}
+        </tr>
+      ))}
+    </tfoot>
+  );
+}
+
+function AvailabilityCell(props) {
+  const [isPending, startTransition] = useTransition();
+
+  return (
+    <input
+      type={"checkbox"}
+      disabled={isPending}
+      checked={props.getValue()}
+      onChange={(e) => {
+        startTransition(() =>
+          editIngredient(props.row.id, { available: e.target.checked })
+        );
+      }}
+    />
   );
 }
