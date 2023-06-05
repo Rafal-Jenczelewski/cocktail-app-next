5ce1556aa5fdb3d5b527c7a3bf5932b0c7a21f3a
rjenc <r.jenczelewski@gmail.com>
2023-06-05 22:48:54 +0200

    Title + favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Cocktails",
+  title: "Bartender",
 };
 
 export default function RootLayout({
