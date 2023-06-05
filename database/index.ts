2c715a7a96e0f1ed667be6336db966b3038ee63d
rjenc <r.jenczelewski@gmail.com>
2023-06-05 23:39:06 +0200

    Trying to reuse Remix code for prisma for perf

--- database/index.ts
+++ database/index.ts
@@ -1,10 +1,26 @@
 import { PrismaClient } from "@prisma/client";
 
-//@ts-ignore
-if (!global.prisma) {
-  //@ts-ignore
-  global.prisma = new PrismaClient();
+let prisma: PrismaClient;
+
+declare global {
+  var __db__: PrismaClient;
 }
 
-//@ts-ignore
-export const prisma = global.prisma;
+// this is needed because in development we don't want to restart
+// the server with every change, but we want to make sure we don't
+// create a new connection to the DB with every change either.
+// in production we'll have a single connection to the DB.
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  //@ts-ignore
+  if (!global.__db__) {
+    //@ts-ignore
+    global.__db__ = new PrismaClient();
+  }
+  //@ts-ignore
+  prisma = global.__db__;
+  prisma.$connect();
+}
+
+export { prisma };
