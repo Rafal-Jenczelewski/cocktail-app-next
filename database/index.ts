48377f142d3ffc67fe18134c4b66ce9add762525
rjenc <r.jenczelewski@gmail.com>
2023-06-05 23:23:51 +0200

    Ignoring stuff in database cause i dont know TS

--- database/index.ts
+++ database/index.ts
@@ -1,7 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
+//@ts-ignore
 if (!global.prisma) {
+  //@ts-ignore
   global.prisma = new PrismaClient();
 }
 
+//@ts-ignore
 export const prisma = global.prisma;
